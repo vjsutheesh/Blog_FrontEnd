df13fc907b68623aab1309098a4667bc43839af5
vjsutheesh <vjsutheesh@gmail.com>
2023-11-04 17:32:36 +0530

    anchor tag updated

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
             <h1> The VJ Blog </h1>
             <div className="links">
                 <a href="/"> Home </a>
-                <a href="/Create"> New Blog </a>
+                <a href="/create"> New Blog </a>
             </div>
         </nav>
      );
