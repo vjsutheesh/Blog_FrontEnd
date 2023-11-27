25cdf786f7cdb759f65eb64875bb18c1ffbba126
vjsutheesh <vjsutheesh@gmail.com>
2023-11-27 20:35:07 +0530

    Navlink

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom/cjs/react-router-dom';
 const Navbar = () => {
     return ( 
-        <nav className="navbar">
+        <NavLink className="navbar">
             <h1> The VJ Blog </h1>
             <div className="links">
                 <Link to="/"> Home </Link>
                 <Link to="/create"> New Blog </Link>
             </div>
-        </nav>
+        </NavLink>
      );
 }
  
